1f1c52b3030d19ad2caae2f653d7d8c01c22c729
Eve Reichmann <ereichmann4@aol.com>
2020-05-22 20:52:01 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,15 @@
 def snake_it_up(string)
   if string[0] == "s"
     "s" + string
-   
+    "s" + string
+    "s" + string
+    "s" + string
+    "s" + string
+    "s" + string
+    "s" + string
+    "s" + string
+    "s" + string
+    "s" + string
   else
     string
   end
